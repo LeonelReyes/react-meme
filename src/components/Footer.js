860595334d59bf687785472f015b8f8dfebd645b
Leonel Isaac Reyes <72421131+LeonelReyes@users.noreply.github.com>
2022-12-26 15:13:21 -0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../stylesheets/footer.css';
+const Footer = () => {
+	return (
+		<footer>
+			Built By{' '}
+			<a
+				href='https://github.com/LeonelReyes'
+				target='_blank'
+				rel='noreferrer noopener'>
+				Leonel Reyes
+			</a>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/navbar.css';
+
+const Navbar = () => {
+	return (
+		<nav className='teal'>
+			
+			<p className='brand-logo'>Meme Generator 2.0</p>
+		</nav>
+	);
+};
+
+export default Navbar;
